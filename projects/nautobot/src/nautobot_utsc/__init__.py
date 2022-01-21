aff1fd06d787b8d643ca1d1917d3cbccd3c221af
Alex Tremblay <alex.tremblay@utoronto.ca>
2022-01-21 10:10:53 -0500

    fix[nautobot]!: convert project to plain package
    convert nautobot project from pep420 namepsace package to plain package.
    Nautobot does not currently support namespaced plugins. https://github.com/nautobot/nautobot/issues/1287
    
    Also reverse project layout. remove nautobot git submodule from utsc-tools, add utsc-tools as submodule to nautobot

---
+++ projects/nautobot/src/nautobot_utsc/__init__.py
@@ -0,0 +1,28 @@
+from importlib import metadata
+
+__version__ = metadata.version(__name__)
+
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class UTSCPluginConfig(PluginConfig):
+    name = "nautobot_utsc"
+    verbose_name = "UTSC Nautobot Plugin"
+    author = "Alex Tremblay"
+    author_email = "alex.tremblay@utoronto.ca"
+    version = __version__
+    description = "A Plugin containing all the extensions and customizations to Nautobot that the UTSC networking team needs"
+    base_url = "utsc"
+    min_version = "0.9"
+    max_version = "9.0"
+    middleware = []
+    installed_apps = []
+    default_settings = {}
+
+    # URL reverse lookup names
+    home_view_name = "plugins:utsc:home"
+    config_view_name = "plugins:utsc:config"
+
+
+config = UTSCPluginConfig
