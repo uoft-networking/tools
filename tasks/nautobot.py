54b268ccf7822789e936bbf67bb4ba9596f818b5
Alex Tremblay <alex.tremblay@utoronto.ca>
2022-08-26 21:28:48 -0400

    dev workflow improvements

--- tasks/nautobot.py
+++ tasks/nautobot.py
@@ -1,5 +1,7 @@
 import os
-from invoke import task, call, Context
+from invoke import task, Context
+
+from tasks.common import needs_sudo
 
 
 PROD_SERVICES = ["nautobot", "nautobot-scheduler", "nautobot-worker"]
@@ -7,21 +9,22 @@ DEV_SERVICES = ["nautobot-dev", "nautobot-dev-scheduler", "nautobot-dev-worker"]
 
 
 @task()
-def start(ctx: Context):
-    with ctx.cd("projects/nautobot"):
-        ctx.run("direnv exec . nautobot-server runserver --noreload")
+def server(c: Context, cmdline: str):
+    with c.cd("projects/nautobot"):
+        c.run(f"direnv exec . nautobot-server {cmdline}")
+
 
 @task()
-def server(ctx: Context, cmdline: str):
-    with ctx.cd("projects/nautobot"):
-        ctx.run(f"direnv exec . nautobot-server {cmdline}")
+def start(c: Context):
+    server(c, "runserver --noreload")
+
 
 @task(
     help={
         "action": "any valid systemd action, or the special actions 'edit' and 'tail'"
     }
 )
-def systemd(ctx: Context, action: str, prod: bool = False):
+def systemd(c: Context, action: str, prod: bool = False):
     """
     Run systemd commands on the services
     """
@@ -29,17 +32,18 @@ def systemd(ctx: Context, action: str, prod: bool = False):
     services = PROD_SERVICES if prod else DEV_SERVICES
     if action == "edit":
         services = " ".join([f"/etc/systemd/system/{s}.service" for s in services])
-        ctx.run(f"sudoedit {services}")
+        c.run(f"sudoedit {services}")
     elif action == "tail":
         services = " ".join([f"-u {s}" for s in services])
-        ctx.run(f"sudo journalctl -f {services}")
+        c.run(f"sudo journalctl -f {services}")
     else:
-        ctx.run(f"sudo systemctl {action} {' '.join(services)}")
+        c.run(f"sudo systemctl {action} {' '.join(services)}")
 
 
 @task()
-def prod_shell(ctx: Context):
-    ctx.run("sudo -u nautobot env -C /opt/nautobot bash --login", pty=True)
+def prod_shell(c: Context):
+    needs_sudo(c)
+    c.sudo("env -C /opt/nautobot bash --login", pty=True, user='nautobot')
 
 
 def _parse_built_files(output: str) -> tuple[str, str]:
@@ -50,36 +54,38 @@ def _parse_built_files(output: str) -> tuple[str, str]:
 
 
 @task()
-def deploy_to_prod(ctx: Context):
-    r = ctx.run("inv build core")
+def deploy_to_prod(c: Context):
+    r = c.run("inv build core")
     _, core_wheel = _parse_built_files(r.stdout)
-    r = ctx.run("inv build nautobot")
+    r = c.run("inv build nautobot")
     _, nautobot_wheel = _parse_built_files(r.stdout)
-    ctx.run("inv nautobot.systemd --prod stop")
+    systemd(c, "stop", prod=True)
     wheels = f"dist/{core_wheel} dist/{nautobot_wheel}"
-    ctx.run(f"gpipx runpip nautobot install --upgrade {wheels}")
-    ctx.run(
+    needs_sudo(c)
+    c.sudo(f"gpipx runpip nautobot install --upgrade {wheels}")
+    c.sudo(
         f"gpipx runpip nautobot install --upgrade --force-reinstall --no-deps {wheels}"
     )
-    ctx.run(
-        "sudo cp projects/nautobot/dev_data/nautobot_config.py /opt/nautobot/nautobot_config.py"
+    c.sudo(
+        "cp projects/nautobot/dev_data/nautobot_config.py /opt/nautobot/nautobot_config.py"
     )
-    ctx.run("sudo chown nautobot:nautobot /opt/nautobot/nautobot_config.py")
-    ctx.run("sudo chmod 644 /opt/nautobot/nautobot_config.py")
-    ctx.run("sudo -iu nautobot direnv exec /opt/nautobot nautobot-server migrate")
-    ctx.run("inv nautobot.systemd --prod start")
-    ctx.run("inv nautobot.systemd --prod status")
-
-
-@task(pre=[call(systemd, "stop")], post=[call(systemd, "start")])
-def db_refresh(ctx: Context):
-    ctx.run("/opt/backups/db/actions sync_prod_to_dev")
-    with ctx.cd("projects/nautobot"):
-        ctx.run("nautobot-server migrate")
+    c.sudo("chown nautobot:nautobot /opt/nautobot/nautobot_config.py")
+    c.sudo("chmod 644 /opt/nautobot/nautobot_config.py")
+    c.run("sudo -iu nautobot direnv exec /opt/nautobot nautobot-server migrate")
+    systemd(c, "start", prod=True)
+    systemd(c, "status", prod=True)
 
 
 @task()
-def curl_as(ctx: Context, endpoint: str, user: str = "me", prod: bool = False, method="GET"):
+def db_refresh(c: Context):
+    systemd(c, "stop")
+    c.run("/opt/backups/db/actions sync_prod_to_dev")
+    c.run("inv nautobot.server migrate")
+    systemd(c, "start")
+
+
+@task()
+def curl_as(c: Context, endpoint: str, user: str = "me", prod: bool = False, method="GET"):
     if user == "me":
         token = os.environ["MY_API_TOKEN"]
     else:
@@ -88,6 +94,6 @@ def curl_as(ctx: Context, endpoint: str, user: str = "me", prod: bool = False, m
         url = "https://engine.netmgmt.utsc.utoronto.ca/api"
     else:
         url = "https://dev.engine.netmgmt.utsc.utoronto.ca/api"
-    ctx.run(
+    c.run(
         f"curl -H 'Authorization: Token {token}' -H 'Accept: application/json;' -H 'Content-Type: application/json' -X {method} {url}/{endpoint}"
     )
