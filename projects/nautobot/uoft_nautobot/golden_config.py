b63bae937ef8e32d1e8ebb2dcbe3c26d5fa5033d
Alex Tremblay <alex.tremblay@utoronto.ca>
2022-10-25 10:11:56 -0400

    work

--- projects/nautobot/uoft_nautobot/golden_config.py
+++ projects/nautobot/uoft_nautobot/golden_config.py
@@ -1,35 +1,6 @@
 from box import Box
 from netaddr import IPNetwork
 
-def annotate_intf_ip(intf):
-    intf.ip = Box()
-    intf.ip.v4 = []
-    intf.ip.v6 = []
-    for addr in intf.ip_addresses:
-        addr = IPNetwork(addr.address)
-        if addr.version == 4:
-            intf.ip.v4.append(addr)
-        else:
-            intf.ip.v6.append(addr)
-    return intf
-
-def annotate_intf_type(intf):
-    if intf.mgmt_only:
-        intf.type = "management"
-    elif intf.enabled is False:
-        intf.type = "disabled"
-    elif intf.name.startswith("Port-channel"):
-        intf.type = "port-channel"
-    elif intf.lag:
-        intf.type = "port-channel-member"
-    elif intf.label and "uplink" in intf.label.lower():
-        intf.type = "uplink"
-    elif "tagged" in intf.vlan_mode.lower():
-        intf.type = "trunk"
-    else:
-        intf.type = "other"
-    return intf
-
 def process_interfaces(d):
     
     def yield_interfaces():
@@ -58,14 +29,45 @@ def process_interfaces(d):
             res.extend(l)
         yield from res
 
+    def annotate_intf_ip(intf):
+        intf.ip = Box()
+        intf.ip.v4 = []
+        intf.ip.v6 = []
+        for addr in intf.ip_addresses:
+            addr = IPNetwork(addr.address)
+            if addr.version == 4:
+                intf.ip.v4.append(addr)
+            else:
+                intf.ip.v6.append(addr)
+        return intf
+
+    def annotate_intf_type(intf):
+        if intf.mgmt_only:
+            intf.type = "management"
+        elif intf.enabled is False:
+            intf.type = "disabled"
+        elif intf.name.startswith("Port-channel"):
+            intf.type = "port-channel"
+        elif intf.lag:
+            intf.type = "port-channel-member"
+        elif intf.label and "uplink" in intf.label.lower():
+            intf.type = "uplink"
+        elif intf.vlan_mode and 'tagged' in intf.vlan_mode.lower():
+            intf.type = "trunk"
+        else:
+            intf.type = "other"
+        return intf
+
     def annotate_interfaces(intfs):
         for intf in intfs:
-            int = annotate_intf_ip(intf)
-            intf["uplink"] = "uplink" in intf.label.lower()
+            intf = annotate_intf_ip(intf)
+            intf = annotate_intf_type(intf)
             yield intf
     
     d.all_interfaces = list(annotate_interfaces(sort_interfaces(yield_interfaces())))
     return d
+
+
 
 def transposer(data):
     d = Box(data)
