648d166faf802f6701d6263737e9d033de0f133a
Kyle Fozo <kyle.fozo@utoronto.ca>
2022-11-16 12:16:09 -0500

    Update checkout.py
    Removed default location actually being applied, for now, its code can stay IMO.  Need to consider this, but I don't see an actual production need for a default location.  What actually needs to happen is to query snipe for the locations in the system and then present that as the choices to the user, ideally I don't want to hard code any of that.
    
    The Default model can stay for now, as i mainly expect to be using this for AP535s now, but same thing, I expect to update this to pull the models from your snipe instance, and provide them as a list of choices.

--- projects/snipeit/uoft_snipeit/checkout.py
+++ projects/snipeit/uoft_snipeit/checkout.py
@@ -4,8 +4,6 @@ from .api import SnipeITAPI
 
 def snipe_checkout_asset(asset: int, location_id: int = None): # type: ignore
     s = settings()
-    if location_id is None:
-        location_id = s.default_assigned_location_id
     
     api = SnipeITAPI.from_settings(s)
     print(f"Asset {asset} checkout:")
