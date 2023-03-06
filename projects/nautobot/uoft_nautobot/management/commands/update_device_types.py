7b5aa0a6732545d19915a0b965c86235534852e6
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-03-06 17:53:52 -0500

    update update_device_types.py

--- projects/nautobot/uoft_nautobot/management/commands/update_device_types.py
+++ projects/nautobot/uoft_nautobot/management/commands/update_device_types.py
@@ -4,39 +4,43 @@ from shutil import which
 from subprocess import PIPE, run
 from typing import Optional
 
+
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from nautobot.extras.models import GitRepository
+from nautobot.utilities.utils import NautobotFakeRequest
+from nautobot.users.models import User
 from ...datasources import refresh_single_device_type
 
 
 class Command(BaseCommand):
-    help = "Create or update a DeviceType entry in the database, from a 'netbox-community/devicetype-library' compatible git repo"
+    help = "Create or update DeviceType entries in the database, from a 'netbox-community/devicetype-library' compatible git repo"
 
-    def add_arguments(self, parser: ArgumentParser):
-        def_git_path = f"{settings.GIT_ROOT}/netbox-community-devicetype-library"
-        parser.add_argument(
-            "--git-repo-path",
-            metavar='PATH',
-            default=def_git_path,
-            help="filesystem path of a git repository containing DeviceType definition yaml files",
+    def handle(self, *args, **options):
+        repo_name = "devicetype-library"
+        url = "https://github.com/netbox-community/devicetype-library"
+        repo = GitRepository.objects.filter(name=repo_name).first()
+    
+
+
+        request = NautobotFakeRequest(
+            {
+                "user": User.objects.get(username='trembl94'),
+                "path": "/extras/git-repositories/",
+                "META": {"REMOTE_ADDR": ""},
+                "GET": {},
+                "POST": {},
+            }
         )
-        parser.add_argument(
-            "type_definitions",
-            metavar='YAML_FILE',
-            nargs="*",
-            help="name of a yaml file to process, relative to the git repo. If none are specified and `fzf` is intalled on this system, an interactive prompt will be opened to select one",
-        )
 
-    def handle(self, *args, git_repo_path: str | Path = '', type_definitions: Optional[list[str]] = None, **options):
-        type_definitions = type_definitions or []
-        git_repo_path = Path(git_repo_path)
-        if not type_definitions:
-            if which('fzf') is None:
-                raise CommandError('no DEVICE_TYPE specified. fzf utility is not installed, so DEVICE_TYPE cannot be specified interactively. please run this command again with --help')
-
-            res = run('fzf -m'.split(), check=True, stdout=PIPE, cwd=git_repo_path)
-            type_definitions.extend(res.stdout.decode().splitlines())
-        
-        for filename in type_definitions:
-            dt = refresh_single_device_type(git_repo_path / filename)
-            self.stderr.write(f'Created device "{dt}" from file `{filename}`')
+        if repo:
+            repo.request = request
+            
+            from ...datasources import refresh_device_types
+            from unittest.mock import MagicMock
+            refresh_device_types(repo, MagicMock(), interactive=True)
+            #repo.save()  # Force a refresh, including the processing of datasource callbacks
+        else:
+            repo = GitRepository.objects.create(
+                name=repo_name, branch="master", remote_url=url, provided_contents=["nautobot.device_types"], request = request
+            )
