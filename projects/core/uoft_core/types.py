167610b80a9cf49c2c3588bedfebca26ca421b98
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-03-06 17:44:48 -0500

    feat(core): add pydantic-compatible IPNetwork and IPAddress types from netaddr to the core types

--- projects/core/uoft_core/types.py
+++ projects/core/uoft_core/types.py
@@ -3,6 +3,24 @@ from typing import Literal
 from pathlib import Path
 from pydantic import BaseModel
 from pydantic.types import SecretStr, FilePath, DirectoryPath
+try:
+    from netaddr import IPNetwork as IPNetworkBase, IPAddress as IPAddressBase
+
+    class IPNetwork(IPNetworkBase):
+        @classmethod
+        def __get_validators__(cls):
+            def validator(val: str) -> "IPNetwork":
+                return cls(val)
+            yield validator
+    
+    class IPAddress(IPAddressBase):
+        @classmethod
+        def __get_validators__(cls):
+            def validator(val: str) -> "IPAddress":
+                return cls(val)
+            yield validator
+except ImportError:
+    pass
 
 # All data types that we endeavor to support in uoft_core
 # TODO: merge types from switchconfig.types into here
