2c1a650e3b6be1591f3f1c333c4280c2cb3ca003
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-04-12 15:36:46 -0400

    add git.* tasks to invoke

--- tasks/git.py
+++ tasks/git.py
@@ -1,6 +1,6 @@
 import os
 import re
-from invoke import task, Context
+from invoke import task, Context, Result
 
 from tasks.common import needs_sudo
 
@@ -8,7 +8,9 @@ from tasks.common import needs_sudo
 def merge_from_main(c: Context):
     """pull changes to main branch and merge into current branch"""
     my_branch = os.environ.get("MY_BRANCH", input("Enter branch name: "))
-    # TODO: check and make sure 
+    res: Result = c.run("git status")
+    if "nothing to commit, working tree clean" not in res.stdout:
+        raise Exception("Working tree is not clean, please commit or stash changes")
     c.run("git checkout main")
     c.run("git pull")
     c.run(f"git checkout {my_branch}")
