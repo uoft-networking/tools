f236181aa6984720f5911e965952e49560c92e9d
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-06-18 21:05:51 -0400

    add support for config parsing

--- dev_docs/myscript.py
+++ dev_docs/myscript.py
@@ -1,21 +1,27 @@
 from argparse import ArgumentParser
+from configparser import ConfigParser
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 parser = ArgumentParser()
-parser.add_argument("host", help="The host to connect to")
-parser.add_argument("username", help="The username for the user")
-parser.add_argument("password", help="The password for the user")
+parser.add_argument("--host", default=None, help="The host to connect to")
+parser.add_argument("--username", default=None, help="The username for the user")
+parser.add_argument("--password", default=None, help="The password for the user")
 args = parser.parse_args()
 
-host = args.host
-username = args.username
-password = args.password
+config = ConfigParser()
+config.read("config.ini")
+host = config["DEFAULT"]["host"]
+username = config["DEFAULT"]["username"]
+password = config["DEFAULT"]["password"]
+
+host = args.host if args.host else host
+username = args.username if args.username else username
+password = args.password if args.password else password
 
 response = requests.get(
-    f"https://{host}/api/v1/data",
-    auth=HTTPBasicAuth(username, password)
+    f"https://{host}/api/v1/data", auth=HTTPBasicAuth(username, password)
 )
 mydata = response.json()["the-data-i-want"]
 
