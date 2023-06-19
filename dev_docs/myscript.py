67f1253c14012a6446ad3f830be0648d3fc5d861
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-06-18 20:46:10 -0400

    add arg parse support

--- dev_docs/myscript.py
+++ dev_docs/myscript.py
@@ -1,12 +1,20 @@
+from argparse import ArgumentParser
+
 import requests
 from requests.auth import HTTPBasicAuth
 
-host = "example-server.utoronto.ca"
-username = "myusername"
-password = "mypassword"
+parser = ArgumentParser()
+parser.add_argument("host", help="The host to connect to")
+parser.add_argument("username", help="The username for the user")
+parser.add_argument("password", help="The password for the user")
+args = parser.parse_args()
+
+host = args.host
+username = args.username
+password = args.password
 
 response = requests.get(
-    f"https://{host}/api/v1/data", 
+    f"https://{host}/api/v1/data",
     auth=HTTPBasicAuth(username, password)
 )
 mydata = response.json()["the-data-i-want"]
