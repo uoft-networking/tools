fe291dc81427776bb79e2d8f7dd4dbdcace54613
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-08-15 22:41:25 -0400

    [snipeit] bugfixes, code cleanup
    Co-authored-by: Kyle Fozo <kyle.fozo@utoronto.ca>

--- projects/snipeit/uoft_snipeit/print.py
+++ projects/snipeit/uoft_snipeit/print.py
@@ -4,5 +4,5 @@ from uoft_core import shell
 def system_print_label():
     print("Printing asset label")
     shell(
-        rf"lp -d QL-800 -o media=29x90mm -o scaling=0 -o MediaType=Labels\ *Tape -o AutoCut=False -o AutoEject=False -P 1 ~/Asset-Label.jpg"
+        rf"lp -d QL-800 -o landscape -o media=29x90mm -o scaling=100 -o MediaType=Labels\ *Tape -o AutoCut=False -o AutoEject=False -P 1 ~/Asset-Label.jpg"
     )
