e0fb0eaaf29ef26414d7d060a5e4e2775ee6d47c
Kyle-Fozo <kyle.fozo@utoronto.ca>
2023-11-20 14:28:48 -0500

    [aruba] Add list-aps command used by ansible scripts.

---
+++ projects/aruba/uoft_aruba/cli_commands/list_aps.py
@@ -0,0 +1,20 @@
+import typer
+import pprint
+from .. import settings
+
+run = typer.Typer(  # If run as main create a 'typer.Typer' app instance to run the program within.
+    context_settings={"max_content_width": 120, "help_option_names": ["-h", "--help"]},
+    no_args_is_help=True,
+    help=__doc__,  # Use this module's docstring as the main program help text
+    short_help="Aruba AP List Tool",
+    add_completion=False,
+)
+
+@run.command()
+def List_APs():
+    s = settings()
+    with s.mm_api_connection as host:
+        output = host.showcommand("show whitelist-db cpsec")[
+        "Control-Plane Security Allowlist-entry Details"
+    ]
+        pprint.pprint(output)
