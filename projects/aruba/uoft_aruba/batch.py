b5f2296908374bb44d065cfd5ec79a3a9c1cd3fb
Alex Tremblay <alex.tremblay@utoronto.ca>
2024-01-05 13:30:27 -0500

    [aruba] refactor cpsec-whitelest command

---
+++ projects/aruba/uoft_aruba/batch.py
@@ -0,0 +1,249 @@
+import re
+from . import Settings
+from typing import Iterable, Sequence, Literal
+from functools import cached_property
+
+from uoft_core.types import BaseModel
+from pydantic import validator
+
+
+class AP(BaseModel):
+    name: str
+    group: str
+    mac_address: str
+
+    @validator("mac_address")
+    def validate_mac_address(cls, v):
+        mac_address_validate_pattern = re.compile(
+            r"^(?:[0-9A-Fa-f]{2}[:\-\.]?){5}(?:[0-9A-Fa-f]{2})$"
+        )
+        match = re.match(mac_address_validate_pattern, v)
+        if match:
+            v = re.sub("[:.-]", "", v)
+            v = ":".join([v[i : i + 2] for i in range(0, 12, 2)])
+            v = v.lower()
+            return v
+        else:
+            raise ValueError("Invalid MAC Address")
+
+    @validator("name", "group")
+    def validate_len(cls, v):
+        if len(v) > 75:
+            raise ValueError("too long, must be 75 characters or less")
+        return v
+
+
+class ProvisionError(Exception):
+    def __init__(self, *args: object, ap: AP) -> None:
+        self.ap = ap
+        super().__init__(*args)
+
+
+class InvalidField(ProvisionError):
+    def __init__(self, *args: object, field: str, ap: AP) -> None:
+        self.field = field
+        super().__init__(*args, ap=ap)
+
+
+class AlreadyExists(ProvisionError):
+    def __init__(self, *args: object, ap: AP, old_ap: dict[str, str]) -> None:
+        self.old_ap = old_ap
+        super().__init__(*args, ap=ap)
+
+
+class Provisioner:
+    def __init__(
+        self,
+        on_error: Literal["skip", "stop", "force"] = "skip",
+        dry_run: bool = False,
+    ) -> None:
+        self.settings = Settings.from_cache()
+        self.error_policy = on_error
+        self.dry_run = dry_run
+        self._controller = None
+        self._mobility_master = None
+
+    def provision_aps(
+        self,
+        inputs: Sequence[tuple[str, str, str]],
+    ):
+        return list(self.provision_aps_iter(inputs))
+
+    def provision_aps_iter(
+        self,
+        inputs: Iterable[tuple[str, str, str]],
+    ):
+        for ap in inputs:
+            try:
+                yield self.provision_ap(*ap)
+            except InvalidField as e:
+                if self.error_policy == "skip":
+                    self.console.print(
+                        f"[red]ERROR[/] {e.args[0]} [red]SKIPPING[/] {e.ap}"
+                    )
+                    yield e
+                else:
+                    raise e
+            except AlreadyExists as e:
+                if 'already correctly provisioned' in e.args[0]:
+                    self.console.print(
+                        f"AP_NAME {e.ap.name} already correctly provisioned. [green]SKIPPING[/]"
+                    )
+                    yield e.ap
+                elif self.error_policy == "force":
+                    yield self.force_provision_ap(e.ap, e.old_ap)
+                elif self.error_policy == "skip":
+                    self.console.print(
+                        f"[red]ERROR[/] {e.args[0]} [red]SKIPPING[/] {e.ap}"
+                    )
+                    yield e
+                else:
+                    raise e
+
+    def force_provision_ap(self, ap: AP, old_ap: dict[str, str]):
+        self.delete_existing_ap(old_ap)
+        return self.provision_ap(ap.name, group=ap.group, mac_address=ap.mac_address)
+
+    def delete_existing_ap(self, old_ap):
+        msg = f"Deleted existing AP_NAME {old_ap['AP-Name']} / {old_ap['MAC-Address']} from whitelist...[green]GOOD[/]"
+        if self.dry_run:
+            msg = "Would have " + msg
+        else:
+            self.mobility_master.ap_provisioning.wdb_cpsec_delete_mac(
+                old_ap['MAC-Address']
+            )
+        self.console.print(msg)
+
+    def provision_ap(self, name, group, mac_address):
+        self.console.print(
+            f"Provisioning {name} with MAC {mac_address} in group {group}..."
+        )
+
+        # instantiating the AP will validate the fields automatically. Thanks Pydantic!
+        ap = AP(mac_address=mac_address, group=group, name=name)
+        self.console.print("Verifying input parameters...[green]GOOD[/]")
+
+        self._validate_group(ap)
+
+        self._validate_name(ap)
+
+        self._validate_mac(ap)
+
+        msg = f"Added new AP_NAME {ap.name} / {ap.mac_address} to whitelist...[green]GOOD[/]"
+        if self.dry_run:
+            msg = "Would have " + msg
+        else:
+            self.mobility_master.ap_provisioning.wdb_cpsec_add_mac(
+                ap.mac_address, ap.group, ap.name
+            )
+        self.console.print(msg)
+
+        msg = f"Modified CPSEC entry for {ap.name} / {ap.mac_address} to factory_approved...[green]GOOD[/]"
+        if self.dry_run:
+            msg = "Would have " + msg
+        else:
+            self.mobility_master.ap_provisioning.wdb_cpsec_modify_mac_factory_approved(
+                ap.mac_address
+            )
+        self.console.print(msg)
+
+        return ap
+
+    def __del__(self):
+        # Make sure we log out of the controllers when we're done
+        if self._controller is not None:
+            self._controller.logout()
+        if self._mobility_master is not None:
+            self._mobility_master.logout()
+
+    @property
+    def controller(self):
+        if self._controller is None:
+            # we only need API access to a controller to collect the list of AP groups
+            # it doesn't really matter which controller we use, so we just pick the first one
+            self._controller = self.settings.md_api_connections[0]
+            self._controller.login()
+        return self._controller
+
+    @property
+    def mobility_master(self):
+        if self._mobility_master is None:
+            self._mobility_master = self.settings.mm_api_connection
+            self._mobility_master.login()
+        return self._mobility_master
+
+    def _validate_group(self, ap: AP):
+        if ap.group not in self.all_groups_by_name:
+            msg = f"AP_GROUP {ap.group} does not exist on controller."
+            raise InvalidField(msg, field="ap_group", ap=ap)
+        self.console.print(
+            f"Verifying AP_GROUP {ap.group} exists on controller...[green]GOOD[/]"
+        )
+
+    def _validate_name(self, ap: AP):
+        if ap.name in self.existing_aps_by_name:
+            existing_ap = self.existing_aps_by_name[ap.name]
+            if existing_ap["MAC-Address"] == ap.mac_address:
+                if existing_ap["AP-Group"] == ap.group:
+                    raise AlreadyExists(
+                        f"AP_NAME {ap.name} is already correctly provisioned.",
+                        ap=ap,
+                        old_ap=existing_ap,
+                    )
+                else:
+                    msg = f"AP_NAME {ap.name} already exists on controller in group {existing_ap['AP-Group']}."
+                    raise AlreadyExists(msg, ap=ap, old_ap=existing_ap)
+            else:
+                msg = f"AP_NAME {ap.name} already exists on controller with MAC {existing_ap['MAC-Address']}."
+                raise AlreadyExists(msg, ap=ap, old_ap=existing_ap)
+        self.console.print(
+            f"Verifying AP_NAME {ap.name} is not in use...[green]GOOD[/]"
+        )
+
+    def _validate_mac(self, ap: AP):
+        if ap.mac_address in self.existing_aps_by_mac:
+            existing_ap = self.existing_aps_by_mac[ap.mac_address]
+            if existing_ap["AP-Name"] == ap.name:
+                if existing_ap["AP-Group"] == ap.group:
+                    raise AlreadyExists(
+                        f"MAC {ap.mac_address} is already correctly provisioned.",
+                        ap=ap,
+                        old_ap=existing_ap,
+                    )
+                else:
+                    msg = f"MAC {ap.mac_address} already exists on controller in group {existing_ap['AP-Group']}."
+                    raise AlreadyExists(msg, ap=ap, old_ap=existing_ap)
+            msg = f"MAC {ap.mac_address} already exists on controller with AP_NAME {existing_ap['AP-Name']}."
+            raise AlreadyExists(msg, ap=ap, old_ap=existing_ap)
+        self.console.print(
+            f"Verifying MAC {ap.mac_address} is not in use...[green]GOOD[/]"
+        )
+
+    @property
+    def console(self):
+        return self.settings.util.console
+
+    @cached_property
+    def all_groups_by_name(self) -> set[str]:
+        return {
+            g["profile-name"].rpartition("'")[2]
+            for g in self.controller.wlan.get_ap_groups()
+        }
+
+    @cached_property
+    def existing_aps_in_whitelist(self):
+        return self.mobility_master.ap_provisioning.get_cpsec_whitelist()
+
+    @cached_property
+    def existing_aps_by_name(self):
+        return {
+            ap["AP-Name"]: ap for ap in self.existing_aps_in_whitelist if ap["AP-Name"]
+        }
+
+    @cached_property
+    def existing_aps_by_mac(self):
+        return {
+            ap["MAC-Address"]: ap
+            for ap in self.existing_aps_in_whitelist
+            if ap["MAC-Address"]
+        }
--- projects/aruba/uoft_aruba/tests/end_to_end_tests.py
+++ projects/aruba/uoft_aruba/tests/end_to_end_tests.py
@@ -1,6 +1,8 @@
 from uoft_aruba import Settings, ArubaRESTAPIClient
+from .. import batch
 
 import pytest
+
 
 def _get_blacklist(*controllers):
     blacklist = []
@@ -8,6 +10,7 @@ def _get_blacklist(*controllers):
         controller: ArubaRESTAPIClient
         blacklist += [x["STA"] for x in controller.wlan.get_ap_client_blacklist()]
     return set(blacklist)
+
 
 @pytest.mark.end_to_end
 class APITests:
@@ -46,7 +49,9 @@ class APITests:
             md2.login()
 
             group_name = mm.wlan.get_ap_groups()[0]["profile-name"]
-            mm.ap_provisioning.wdb_cpsec_add_mac("11:11:11:11:11:11", group_name, "testap")
+            mm.ap_provisioning.wdb_cpsec_add_mac(
+                "11:11:11:11:11:11", group_name, "testap"
+            )
             whitelist = mm.ap_provisioning.get_cpsec_whitelist()
             whitelist = {x["MAC-Address"]: x for x in whitelist}
             assert "11:11:11:11:11:11" in whitelist
@@ -61,3 +66,87 @@ class APITests:
             mm.logout()
             md1.logout()
             md2.logout()
+
+
+@pytest.mark.end_to_end
+def test_batch_provisioner():
+    p = batch.Provisioner(dry_run=True)
+    groups = list(p.all_groups_by_name)
+    valid_group = groups[0]
+    other_group = groups[1]
+    ap_invalid_group = "invalid_group"
+
+    ap_already_provisioned = "already_provisioned", valid_group, "00:1a:1e:00:00:00"
+    ap_to_provision = "to_provision", valid_group, "00:1a:1e:00:00:01"
+    ap_invalid_group = (
+        "invalid_group",
+        ap_invalid_group,
+        "00:1a:1e:00:00:02",
+    )
+    ap_other_group = (
+        "other_group",
+        other_group,
+        "00:1a:1e:00:00:03",
+    )
+    ap_mac_in_use = (
+        "mac_in_use",
+        valid_group,
+        "00:1a:1e:00:00:04",
+    )
+
+    input_list = [
+        ap_already_provisioned,
+        ap_to_provision,
+        ap_invalid_group,
+        ap_other_group,
+        ap_mac_in_use,
+    ]
+
+    # setup
+
+    # we want 'ap_already_provisioned' to already exist in the whitelist
+    # by the time we run the batch provisioner
+    p.mobility_master.ap_provisioning.wdb_cpsec_add_mac(
+        ap_name=ap_already_provisioned[0],
+        ap_group=ap_already_provisioned[1],
+        mac_address=ap_already_provisioned[2],
+    )
+
+    # we want 'ap_other_group' to already exist in the whitelist
+    # but in a different group than the one we're trying to provision it in
+    p.mobility_master.ap_provisioning.wdb_cpsec_add_mac(
+        ap_name=ap_other_group[0],
+        ap_group=valid_group,
+        mac_address=ap_other_group[2],
+    )
+
+    # we want the mac address of 'ap_mac_in_use' to already exist in the whitelist
+    # but with a different ap name than the one we're trying to provision
+    p.mobility_master.ap_provisioning.wdb_cpsec_add_mac(
+        ap_name="already_in_use",
+        ap_group=valid_group,
+        mac_address=ap_mac_in_use[2],
+    )
+
+    try:
+        res = p.provision_aps(input_list)
+    finally:
+        for *_, mac in input_list:
+            try:
+                p.mobility_master.ap_provisioning.wdb_cpsec_delete_mac(mac)
+            except Exception as e:
+                if "Entry does not exist" in e.args[0]:
+                    pass
+                else:
+                    raise e
+
+    assert len(res) == len(input_list)
+    assert isinstance(res[0], batch.AP)
+    assert isinstance(res[1], batch.AP)
+    assert isinstance(res[2], batch.InvalidField)
+    assert res[2].field == "ap_group"
+    assert "does not exist" in res[2].args[0]
+    assert isinstance(res[3], batch.AlreadyExists)
+    assert "already exists on controller in group" in res[3].args[0]
+    assert isinstance(res[4], batch.AlreadyExists)
+    assert "already exists on controller with AP_NAME" in res[4].args[0]
