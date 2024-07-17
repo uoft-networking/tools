31c79611760056b5ceb2a79afd43f4cde6028baf
Alex Tremblay <alex.tremblay@utoronto.ca>
2024-07-17 11:32:23 -0400

    [dev] code cleanup

--- projects/core/uoft_core/tests/integration_tests.py
+++ projects/core/uoft_core/tests/integration_tests.py
@@ -37,39 +37,41 @@ class _File(uoft_core.StrEnum):
     create = object()
     dont_use = object()
 
+
 class _StdLibEnum(enum.Enum):
     choice1 = enum.auto()
     choice2 = enum.auto()
     choice3 = enum.auto()
 
+
 class _SubModel(BaseModel):
     sub_a: str
     sub_b: str
+
 
 class _Example(BaseModel):
     a_bool: bool
     a_str: str
     a_password: SecretStr
     an_int: int
-    a_choice: Literal['one', 'two', 'three']
+    a_choice: Literal["one", "two", "three"]
     an_enum: _File
     another_enum: _StdLibEnum
-    a_path: Path 
-    a_dirpath: DirectoryPath 
+    a_path: Path
+    a_dirpath: DirectoryPath
     a_filepath: FilePath
     a_list_of_str: list[str]
     a_dict_of_str: dict[str, str]
     a_model: _SubModel
 
+
 @pytest.mark.integration
 class PromptTests:
-
     def _wait_for_screen_update(self, child_pty_file_descriptor, vty_input_stream):
         """Wait for the child process to finish writing to "screen", pipe contents to the vty for analysis"""
         while True:
             try:
-                [child_pty_file_descriptor], _, _ = select.select(
-                    [child_pty_file_descriptor], [], [], 1)
+                [child_pty_file_descriptor], _, _ = select.select([child_pty_file_descriptor], [], [], 1)
             except (KeyboardInterrupt, ValueError):
                 # either test was interrupted or the
                 # file descriptor of the child process
@@ -87,12 +89,13 @@ class PromptTests:
     @pytest.mark.skip(reason="This test is not yet implemented")
     def test_unmocked_prompt(self):
         import pyte
+
         # create pseudo-terminal
         pid, fd = os.forkpty()
         if pid == 0:
             # We are now in the child side of the fork
             # Here we will run the program we want to test
-            p = uoft_core.prompt.Prompt(uoft_core.Util('prompt_test').history_cache)
+            p = uoft_core.prompt.Prompt(uoft_core.Util("prompt_test").history_cache)
             res = p.from_model(_Example)
             assert res
             sys.exit(0)
@@ -114,11 +117,11 @@ class PromptTests:
                 print(line)
 
             # now, do some assertions and interact with the child process
-            assert 'a_bool: ' in screen.display[0]
-
+            assert "a_bool: " in screen.display[0]
 
     def test_mocked_prompt(self, monkeypatch: MonkeyPatch, tmp_path: Path):
         """Mock out prompt_toolkit's PromptSession as best we can, and test Prompt against our mock."""
+
         class mockdoc:
             def __init__(self, v) -> None:
                 self.text = v
@@ -129,45 +132,45 @@ class PromptTests:
 
             def prompt(self, message, **kwargs):
                 v = message.value
-                if 'a_bool' in v:
-                    val = 'true'
-                elif 'a_str' in v:
-                    val = 'hello'
-                elif 'a_password' in v:
-                    val = 'secretsecret'
-                elif 'an_int' in v:
-                    val = '1'
-                elif 'a_choice' in v:
-                    val = 'one'
-                elif 'an_enum' in v:
-                    val = 'read'
-                elif 'another_enum' in v:
-                    val = 'choice1'
-                elif 'a_path' in v:
-                    val = str(tmp_path / 'some_file')
-                elif 'a_dirpath' in v:
+                if "a_bool" in v:
+                    val = "true"
+                elif "a_str" in v:
+                    val = "hello"
+                elif "a_password" in v:
+                    val = "secretsecret"
+                elif "an_int" in v:
+                    val = "1"
+                elif "a_choice" in v:
+                    val = "one"
+                elif "an_enum" in v:
+                    val = "read"
+                elif "another_enum" in v:
+                    val = "choice1"
+                elif "a_path" in v:
+                    val = str(tmp_path / "some_file")
+                elif "a_dirpath" in v:
                     val = str(tmp_path)
-                elif 'a_filepath' in v:
-                    p = tmp_path / 'a_real_file'
+                elif "a_filepath" in v:
+                    p = tmp_path / "a_real_file"
                     p.touch()
                     val = str(p)
-                elif 'a_list_of_str' in v:
-                    val = 'one\ntwo\nthree'
-                elif 'an_int_list' in v:
-                    val = '1\n2\n3'
-                elif 'a_dict_of_str' in v:
-                    val = 'one: a\ntwo: b\nthree: c'
-                elif 'a_model.sub' in v:
-                    val = 'some string'
+                elif "a_list_of_str" in v:
+                    val = "one\ntwo\nthree"
+                elif "an_int_list" in v:
+                    val = "1\n2\n3"
+                elif "a_dict_of_str" in v:
+                    val = "one: a\ntwo: b\nthree: c"
+                elif "a_model.sub" in v:
+                    val = "some string"
                 else:
-                    raise Exception('Uhoh, a test case for this type hasn\'t been implemented yet :(')
+                    raise Exception("Uhoh, a test case for this type hasn't been implemented yet :(")
                 val = mockdoc(val)
-                if kwargs.get('validator'):
-                    kwargs['validator'].validate(val)
+                if kwargs.get("validator"):
+                    kwargs["validator"].validate(val)
                 return val.text
 
-        monkeypatch.setattr('uoft_core.prompt.PromptSession', mocksession)
+        monkeypatch.setattr("uoft_core.prompt.PromptSession", mocksession)
 
-        p = uoft_core.prompt.Prompt(uoft_core.Util('prompt_test').history_cache)
+        p = uoft_core.prompt.Prompt(uoft_core.Util("prompt_test").history_cache)
         res = p.from_model(_Example)
         assert res
