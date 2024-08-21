f24171627acab452c85f60848e978fb975caa84b
Alex Tremblay <alex.tremblay@utoronto.ca>
2024-08-21 12:41:17 -0400

    [dev] remove unneeded tests from vendored netaddr lib

--- projects/core/uoft_core/_vendor/netaddr/tests/ip/test_ip.py
+++
@@ -1,8 +0,0 @@
-import weakref
-
-from ... import IPAddress, IPNetwork, IPRange
-
-def test_ip_classes_are_weak_referencable():
-    weakref.ref(IPAddress('10.0.0.1'))
-    weakref.ref(IPNetwork('10.0.0.1/8'))
-    weakref.ref(IPRange('10.0.0.1', '10.0.0.10'))
