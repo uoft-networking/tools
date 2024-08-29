072c44c765ac3fbb737eedce46e46070d5db3f6f
Alex Tremblay <alex.tremblay@utoronto.ca>
2024-08-29 12:04:06 -0400

    [dev] add support for submodules / custom forks to our pip install hooks
    until we find an elegant solution to #29, this is probably the best i can do

--- projects/grist/pyproject.py
+++ projects/grist/pyproject.py
@@ -1,16 +1,108 @@
-from hatchling.metadata.plugin.interface import MetadataHookInterface #noqa
+from hatchling.metadata.plugin.interface import MetadataHookInterface
 from setuptools_scm import get_version
+import os
+import re
+from shutil import which
+import subprocess
+from pathlib import Path
+
+V = get_version(root="../..", relative_to=__file__, version_scheme="post-release")
 
 
 class CustomMetadataHook(MetadataHookInterface):
+    def is_rye_lock(self):
+        # Not a great way to detect that we're being run from `rye lock`,
+        # but pretty much the only effective way i know of
+        return os.environ.get("PROJECT_ROOT")
+
+    def is_release(self):
+        # if v is not a clean version number (ie contains a '.post###' segment or a '+g{hash}' segment),
+        # we are likely in a dev environment or a CI environment or a specific repo commit that's
+        # been locally cloned for installation. In that case, we want to reference all repo-internal
+        # projects by relative direct reference, not by version number
+        return ".post" not in V and "+g" not in V
+
+    def monorepo_root(self):
+        return Path(self.root).parent.parent
+
+    def ensure_submodules_initialized(self):
+        """
+        If this package is being installed through rye, submodules
+        (including all packages in the 'custom-forks' directory),
+        will already be initialized. If it's being installed from source,
+        (either from a downloaaded zip or from a git clone), we need to
+        initialize the submodules ourselves.
+        """
+        if which("git") and self.monorepo_root().joinpath(".git").exists():
+            # no point even attempting to initialize submodules if git isn't installed
+
+            # check to see if submodules are already initialized, by checking to see if any
+            # folder in the 'custom-forks' directory is empty
+            test_package = next(self.monorepo_root().joinpath("custom-forks").iterdir())
+            if next(test_package.iterdir(), None) is None:
+                # all subfolders of the 'custom-forks' directory should be installable packages.
+                # none should be empty. If any are empty, that's a sign that submodules
+                # haven't been initialized
+                subprocess.run(["git", "submodule", "update", "--init", "--recursive"], cwd=self.monorepo_root())
+
+    def local_dependencies(self, metadata: dict):
+        """
+        Returns a list of all uoft_* dependencies specified in this project's metadata
+        """
+
+        for dep in metadata["dependencies"]:
+            if "uoft_" in dep:
+                proj_name = dep.partition(" ")[0].partition("uoft_")[2]
+                proj_dir = self.monorepo_root() / "projects" / proj_name
+                if proj_dir.exists():
+                    yield dep, proj_dir
+            dep_name = re.split(r" |<|>|!|=|~|@|\[", dep)[0]
+            local_dep = self.monorepo_root() / "custom-forks" / dep_name
+            if local_dep.exists():
+                # dep_name split strips out any extras that may exist in the dependency string
+                # ie 'nautobot[dev]' becomes 'nautobot'
+                # we need to add the extras back in to the dependency string if they exist
+                extras = re.search(r"\[.*\]", dep)
+                if extras:
+                    dep = f"{dep_name}{extras.group()}"
+                yield dep, self.monorepo_root() / "custom-forks" / dep_name
+
+    def update_local_dependency_specifications(self, metadata: dict):
+        """
+        for all uoft_* dependencies specified in this project's metadata,
+        if those depenencies are also contained within this project's monorepo,
+        We want to do one of several things:
+
+        If we're cutting a new release (building a sdist or wheel for upload to PYPI),
+        we want to add version numbers to the dependency specifications.
+        ex: if V == '0.1.0', 'uoft_core' becomes 'uoft_core == 0.1.0'
+
+        If we're installing from source (ex `pip install projects/{project}` or `pip install git+https://...`),
+        we want to add relative direct references to the dependency specifications.
+
+        If this hook is being called from within a `rye lock`, we want to do nothing.
+        """
+        deps = {dep: dep for dep in metadata["dependencies"]}
+        for dep, project_dir in self.local_dependencies(metadata):
+            if self.is_rye_lock():
+                # We're running inside of rye lock
+                deps[dep] = dep
+            elif self.is_release():
+                # We're building a release
+                deps[dep] = f"{dep} == {V}"
+            else:
+                # We're installing from source
+                deps[dep] = f"{dep} @ file://{project_dir}"
+
+        metadata["dependencies"] = list(deps.values())
 
     def update(self, metadata: dict):
         """
         This updates the metadata mapping of the `project` table in-place.
         """
-        v = get_version(root='../..', relative_to=__file__, version_scheme='post-release')
-        metadata['version'] = v
-        for dep in metadata['dependencies'].copy():
-            if 'uoft_core' in dep:
-                metadata['dependencies'].remove(dep)
-                metadata['dependencies'].append(f'uoft_core == {v}')
+
+        metadata["version"] = V
+
+        self.ensure_submodules_initialized()
+
+        self.update_local_dependency_specifications(metadata)
