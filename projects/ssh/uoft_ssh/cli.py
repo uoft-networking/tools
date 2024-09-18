ba1d819487cf5b3882fe70bb806cbe920b6e7199
Alex Tremblay <alex.tremblay@utoronto.ca>
2024-09-18 16:57:03 -0400

    [ssh] implement ssh wrapper command

--- projects/ssh/uoft_ssh/cli.py
+++ projects/ssh/uoft_ssh/cli.py
@@ -1,12 +1,15 @@
 """
 A toolkit for working with SSH. Wrappers, Ansible convenience features, Nornir integration, etc
 """
+
 from typing import Annotated, Optional
 import sys
+import socket
 
 import typer
 
 from uoft_core import logging
+from uoft_core.console import console
 from . import Settings
 
 logger = logging.getLogger(__name__)
@@ -26,6 +29,7 @@ def _version_callback(value: bool):
     )
     raise typer.Exit()
 
+
 app = typer.Typer(
     name="ssh",
     context_settings={"max_content_width": 120, "help_option_names": ["-h", "--help"]},
@@ -33,10 +37,23 @@ app = typer.Typer(
     help=__doc__,  # Use this module's docstring as the main program help text
 )
 
+
 @app.callback()
-#@Settings.wrap_typer_command
-#TODO: implement support for exploding submodels in Settings.wrap_typer_command
-def callback(
+# @Settings.wrap_typer_command
+# TODO: implement support for exploding submodels in Settings.wrap_typer_command
+def ssh(
+    ctx: typer.Context,
+    host: Annotated[str, typer.Argument(help="The hostname or IP address of the remote server")],
+    command: Annotated[
+        str | None,
+        typer.Argument(
+            help="The command to run on the remote server. "
+            "Interactive shell will be launched if no command specified",
+        ),
+    ] = None,
+    login_as_admin: Annotated[
+        bool, typer.Option("--login-as-admin", help="Login as admin user (instead of using your utorid)")
+    ] = False,
     version: Annotated[
         Optional[bool],
         typer.Option("--version", callback=_version_callback, is_eager=True, help="Show version information and exit"),
@@ -54,10 +71,75 @@ def callback(
         DEBUG_MODE = True
     logging.basicConfig(level=log_level)
 
-@app.command()
-def wrapper():
-    # TODO: write and embed an expect script here
-    pass
+
+@app.command(context_settings={"ignore_unknown_options": True, "allow_extra_args": True})
+def wrapper(
+    ctx: typer.Context,
+    host: Annotated[str, typer.Argument(help="The hostname or IP address of the remote server")],
+    command: Annotated[
+        str | None,
+        typer.Option(
+            help="The command to run on the remote server. "
+            "Interactive shell will be launched if no command specified"
+        ),
+    ] = None,
+    login_as_admin: Annotated[bool, typer.Option(help="Login as admin user")] = False,
+):
+    import pexpect
+    s = Settings.from_cache()
+    creds = s.admin if login_as_admin else s.personal
+    extra_args = ctx.args
+
+    # sanity check the host
+    try:
+        ip_addr = socket.gethostbyname(host)
+    except socket.gaierror as e:
+        logger.error(f"Could not resolve {host}: \n{e}")
+        sys.exit(1)
+
+    if '-l' in extra_args:
+        l_index = extra_args.index('-l')
+        username = extra_args[l_index + 1]
+        extra_args.pop(l_index+1)
+        extra_args.pop(l_index)
+    else:
+        username = creds.username
+
+    ssh_command = f"ssh {' '.join(extra_args)} {username}@{ip_addr}"
+    if command:
+        ssh_command += f" {command}"
+
+    logger.debug(f"Running command: {ssh_command}")
+
+
+    child = pexpect.spawn(ssh_command)
+    logger.info("Waiting for password prompt")
+
+    # TODO: on timeout, check for host key errors and handle them
+    match = child.expect(["[pP]assword:", 'has changed and you have requested strict checking'])
+    if match == 1:
+        logger.error(f"Host key for {host} has changed")
+        logger.warning("If this is expected, run the following command to remove the old key:")
+        logger.warning(f"[bold]ssh-keygen -R {host}[/]")
+        logger.warning(f"[bold]ssh-keygen -R {ip_addr}[/]")
+        sys.exit(1)
+
+
+    console().set_window_title(f"uoft-ssh {host}")
+    child.sendline(creds.password.get_secret_value())
+    while True:
+        child.interact()
+        if child.isalive():
+            # the only way to get to this point in control flow is to have hit the escape character (default is ^])
+            # The only reason for a user to do so is to inject the enable password into the session
+            child.sendline(s.enable_secret.get_secret_value())
+            continue
+        else:
+            # `child.interact()` returned because the child process exited
+            break
+    child.close()
+    sys.exit(child.exitstatus)
+
 
 @app.command()
 def nornir():
