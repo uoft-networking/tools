039d264a52a68a8a50683f5c7f92abe034817493
Alex Tremblay <alex.tremblay@utoronto.ca>
2025-05-09 16:19:47 -0400

    [scripts] arista: clean up, refactor, flesh out, fix type hint issues

--- projects/scripts/uoft_scripts/arista_cli.py
+++ projects/scripts/uoft_scripts/arista_cli.py
@@ -1,14 +1,7 @@
 from typing import Annotated
-from uoft_core import BaseSettings, SecretStr
 
 import typer
 
-
-class Settings(BaseSettings):
-    cvp_token: SecretStr
-
-    class Config(BaseSettings.Config):
-        app_name = "arista"
 
 
 def prettyprinter_result_callback(result, *args, **kwargs):
@@ -29,18 +22,24 @@ def prettyprinter_result_callback(result, *args, **kwargs):
 
 app = typer.Typer(name="arista", result_callback=prettyprinter_result_callback)
 
-
-
 @app.command()
-def onboard_via_terminal_server(
+def initial_provision(
     switch: Annotated[str, typer.Argument(help="Switch hostname")],
     terminal_server: Annotated[str, typer.Argument(help="Terminal server hostname")],
     port: Annotated[int, typer.Argument(help="Switch port")],
 ):
-    "Setup an Arista switch for onboarding"
-    from . import onboarding
+    """
+    Given a switch hostname, terminal server, and port,
+    connect to the switch via the terminal server
+    and give it the minimum viable config necessary 
+    to get it online and accessible via SSH over OOB
+    (including RADIUS auth)
+    """
+    from . import arista
 
-    onboarding.onboard_via_terminal_server(switch, terminal_server, port)
+    arista.initial_provision(switch, terminal_server, port)
+
+
 
 
 @app.command()
@@ -49,10 +48,15 @@ def wipe_switch(
     port: Annotated[int, typer.Argument(help="Switch port")],
     reenable_ztp_mode: Annotated[bool, typer.Option(help="Reenable ZTP mode")] = False,
 ):
-    "Wipe an Arista switch and reset it to factory defaults"
-    from . import onboarding
+    """
+    Given a terminal server and port,
+    wipe the Arista switch attached to that port
+    and reset it to factory defaults.
+    This is used for testing and debugging.
+    """
+    from . import arista
 
-    onboarding.wipe_switch(terminal_server, port, reenable_ztp_mode)
+    arista.wipe_switch(terminal_server, port, reenable_ztp_mode)
 
 
 @app.command()
@@ -61,9 +65,9 @@ def push_nautobot_config_to_switch(
 ):
     """Given a switch hostname, pull ip_address and intended config from Nautobot
     push config to switch via SSH"""
-    from . import onboarding
+    from . import arista
 
-    onboarding.push_nautobot_config_to_switch(switch)
+    arista.push_nautobot_config_to_switch(switch)
 
 
 def _debug():
