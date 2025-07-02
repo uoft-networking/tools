a804be645429688a12444c8253217d9f7df81a30
Kyle-Fozo <kyle.fozo@utoronto.ca>
2025-07-02 11:59:23 -0400

    [snipeit] tiny bug fix

--- projects/snipeit/uoft_snipeit/create.py
+++ projects/snipeit/uoft_snipeit/create.py
@@ -2,10 +2,11 @@ from . import settings
 from .api import SnipeITAPI
 
 
-def snipe_create_asset(mac_addr: str, name: str, serial: str, model_id: int) -> int:  # type: ignore
+def snipe_create_asset(mac_addr_raw: str, name: str, serial: str, model_id: int | None = None) -> int:  # type: ignore
     s = settings()
-
     api = SnipeITAPI.from_settings(s)
+    mac_addr = ":".join(mac_addr_raw[i : i + 2] for i in range(0, len(mac_addr_raw), 2))
+    assert model_id
     asset = api.create_asset(model_id, mac_addr, name, serial)
     print(f"New asset ID is {asset:0>5}.")
     return asset
